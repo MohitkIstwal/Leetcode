2e154f7fb30c208076b6c3b25b478fda7563b2dd
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-07-10 00:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (42.10%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        auto it=lower_bound(nums.begin(),nums.end(),target);
+        if(it==nums.end()){
+            return nums.size();
+        }
+        return it-nums.begin();
+    }
+};
