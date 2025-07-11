e2157d4836e8c97a767363369e47127cba1d91cf
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-07-11 15:22:39 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (56.66%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* getknode(ListNode* temp,int k){
+        k-=1;
+        while(temp!=NULL && k>0){
+            k--;
+            temp=temp->next;
+        }
+        return temp;
+    }
+
+    void reversell(ListNode* head){
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* next=NULL;
+        while(curr){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr; curr=next;
+        }
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* temp=head;
+        ListNode* prevNode=NULL;
+        ListNode* nextNode=NULL;
+        int k=2;
+        while(temp!=NULL){
+            ListNode* knode=getknode(temp,k);
+            if(knode==NULL){
+                if(prevNode){
+                    prevNode->next=temp;
+                }
+                break;
+            }
+            else{
+                nextNode=knode->next;
+                knode->next=NULL;
+                reversell(temp);
+                if(temp==head){
+                    head=knode;
+                }
+                else{
+                    prevNode->next=knode;
+                }
+                prevNode=temp;
+                temp=nextNode;
+            }
+        }
+        return head;
+    }
+};
