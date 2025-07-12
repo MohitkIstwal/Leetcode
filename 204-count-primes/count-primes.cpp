470c95226e2363002b3c0d073504728aca72a25f
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-07-13 00:32:02 +0530

    Time: 158 ms (81.66%) | Memory: 132.7 MB (10.90%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -5,10 +5,9 @@ public:
             return 0;
         }
         vector<int> isprime(n,1);
-        isprime[0]=isprime[1]=0;
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<n;i++){
             if(isprime[i]==1){
-                for(int j=2*i;j<n;j+=i){
+                for(int j=i*i;j<n;j+=i){
                     isprime[j]=0;
                 }
             }
