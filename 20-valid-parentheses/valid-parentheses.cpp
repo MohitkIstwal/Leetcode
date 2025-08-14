7fc90d7192ececdb1177af267e63c6ef73896852
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-08-14 18:30:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (60.78%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char ch: s){
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(ch==')'){
+                    if(st.empty() || st.top()!='('){
+                        return false;
+                    }
+                    else{
+                        st.pop();
+                    }
+                }
+                else if(ch=='}'){
+                    if(st.empty() || st.top()!='{'){
+                        return false;
+                    }
+                    else{
+                        st.pop();
+                    }
+                }
+                else{
+                    if(st.empty() || st.top()!='['){
+                        return false;
+                    }
+                    else{
+                        st.pop();
+                    }
+                }
+            }
+            
+        }
+        if(!st.empty()){
+            return false;
+        }
+        return true;
+    }
+};
