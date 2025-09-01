67fe66a951d5fbf16e91b6b7781dabd06e24fa16
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-09-01 13:42:24 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (44.90%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+    //just create 2 arrays of next greater and before greater element . after that jsut add in the ans greedily
+
         int n=height.size();
-        vector<int> premax(n),sufmax(n);
-        premax[0]=height[0];
-        for(int i=1;i<n;i++){
-            premax[i]=max(premax[i-1],height[i]);
-        }
+        vector<int> sufmax(n);
+        // premax[0]=height[0];
+        // for(int i=1;i<n;i++){
+        //     premax[i]=max(premax[i-1],height[i]);
+        // }
         sufmax[n-1]=height[n-1];
         for(int i=n-2;i>=0;i--){
             sufmax[i]=max(sufmax[i+1],height[i]);
         }
-        int ans=0;
+        int ans=0,a=0;
         for(int i=0;i<n;i++){
-            if(min(sufmax[i],premax[i])-height[i]>0){
-                ans+=min(sufmax[i],premax[i])-height[i];
+            a=max(a,height[i]);
+            if(min(sufmax[i],a)-height[i]>0){
+                ans+=min(sufmax[i],a)-height[i];
             }
         }
         return ans;
