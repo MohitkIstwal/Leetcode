4e49a53b669c3e21fc2229fb25fb7cdf5a46304d
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-09-01 13:54:07 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (60.69%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-    //just create 2 arrays of next greater and before greater element . after that jsut add in the ans greedily
+        //the optimal solution is more of a 2 pointer approach
 
-        int n=height.size();
-        vector<int> sufmax(n);
-        // premax[0]=height[0];
-        // for(int i=1;i<n;i++){
-        //     premax[i]=max(premax[i-1],height[i]);
-        // }
-        sufmax[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            sufmax[i]=max(sufmax[i+1],height[i]);
-        }
-        int ans=0,a=0;
-        for(int i=0;i<n;i++){
-            a=max(a,height[i]);
-            if(min(sufmax[i],a)-height[i]>0){
-                ans+=min(sufmax[i],a)-height[i];
+        int lmax,rmax,t,l; lmax=rmax=t=l=0;
+        int r=height.size()-1;
+        while(l!=r){
+            if(height[l]<=height[r]){
+                if(lmax>height[l]){
+                    t+=lmax-height[l];
+                }
+                else{
+                    lmax=height[l];
+                }
+                l++;
+            }
+            else{
+                if(rmax>height[r]){
+                    t+=rmax-height[r];
+                }
+                else{
+                    rmax=height[r];
+                }
+                r--;
             }
         }
-        return ans;
+        return t;
     }
 };
