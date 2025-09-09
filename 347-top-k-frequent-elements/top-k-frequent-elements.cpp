b406552b1601999cfb5fefdde1ead6b75cdc6b65
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-09-09 14:42:35 +0530

    Time: 3 ms (49.72%) | Memory: 17.9 MB (80.95%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -5,9 +5,12 @@ public:
         for(int val: nums){
             mp[val]++;
         }
-        priority_queue<pair<int,int>> pq;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         for(auto it: mp){
             pq.push({it.second,it.first});
+            if(pq.size()>k){
+                pq.pop();
+            }
         }
         vector<int> ans;
         while(k--){
