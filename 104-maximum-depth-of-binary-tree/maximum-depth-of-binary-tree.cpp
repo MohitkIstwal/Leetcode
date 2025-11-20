041e6559532264dc09d0c445354dcb60f93a0de7
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-11-20 22:37:31 +0530

    Time: 3 ms (2.96%) | Memory: 18.9 MB (95.32%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -11,37 +11,14 @@
  */
 class Solution {
 public:
-// doing it using bfs traversal
-    int bfs(TreeNode* root){
-        //vector<vector<int>> ans;
+    int maxDepth(TreeNode* root) {
         if(root==NULL){
             return 0;
         }
-        queue<TreeNode*> q;
-        q.push(root);
-        int ans=1;
-        while(!q.empty()){
-            int size=q.size();
-            vector<int> temp;
-            for(int i=0;i<size;i++){
-                TreeNode* t=q.front();
-                q.pop();
-                //temp.push_back(t->val);
-                if(t->left!=NULL){
-                    q.push(t->left);
-                }
-                if(t->right!=NULL){
-                    q.push(t->right);
-                }
-            }
-            if(!q.empty()){
-                ans++;
-            }
-        }
-        return ans;
-    }
 
-    int maxDepth(TreeNode* root) {
-        return bfs(root);
+        int lh=maxDepth(root->left);
+        int rh=maxDepth(root->right);
+
+        return 1+max(lh,rh);
     }
 };
