908341bfb91e1dac56b842230930ebeed68fbc3d
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-11-20 16:12:38 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (12.85%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+// doing it using bfs traversal
+    vector<vector<int>> bfs(TreeNode* root){
+        vector<vector<int>> ans;
+        if(root==NULL){
+            return ans;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> temp;
+            for(int i=0;i<size;i++){
+                TreeNode* t=q.front();
+                q.pop();
+                temp.push_back(t->val);
+                if(t->left!=NULL){
+                    q.push(t->left);
+                }
+                if(t->right!=NULL){
+                    q.push(t->right);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+
+    int maxDepth(TreeNode* root) {
+        vector<vector<int>> ans=bfs(root);
+        return ans.size();
+    }
+};
