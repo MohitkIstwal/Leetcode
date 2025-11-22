1b22c454e44f27b178b1be56fe3b8842b19249a2
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-11-22 23:06:34 +0530

    Time: 12 ms (64.17%) | Memory: 17.3 MB (66.59%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,40 +9,23 @@
  */
 class Solution {
 public:
-
-    bool getpath(TreeNode* root,int x,vector<TreeNode*> &ans){
-        if(root==NULL){
-            return false;
-        }
-        ans.push_back(root);
-        if(root->val==x){
-            return true;
-        }
-        if(getpath(root->left,x,ans) || getpath(root->right,x,ans)){
-            return true;
-        }
-        ans.pop_back();
-        return false;
-    }
-
-    //using the path method that we learnt in prev question
+    //recursive
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> arr;
-        getpath(root,p->val,arr);
-        vector<TreeNode*> arr1;
-        getpath(root,q->val,arr1);
-        for(int i=0;i<max(arr.size(),arr1.size());i++){
-            if(i==arr.size()){
-                return arr[i-1];
-            }
-            else if(i==arr1.size()){
-                return arr1[i-1];
-            }
-            int a=arr[i]->val,b=arr1[i]->val;
-            if(a!=b){
-                return arr[i-1];
-            }
+        if(root==NULL || p->val==root->val || q->val==root->val){
+            return root;
         }
-        return NULL;
+
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+
+        if(left==NULL){
+            return right;
+        }
+        else if(right==NULL){
+            return left;
+        }
+        else{
+            return root;
+        }
     }
 };
