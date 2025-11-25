fc9523edfc731c6086899e8426fc1ef61b209e2a
Mohit Kumar <148217753+MohitkIstwal@users.noreply.github.com>
2025-11-25 22:38:27 +0530

    Time: 11 ms (15.81%) | Memory: 10.2 MB (13.47%) - LeetSync

---
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    /*
+        so any even number can't be taken 
+        aside from that we have to see
+         for 3 - 111 if the number
+         so other one is 37 who is also 111
+         no ending with 5 also doesnt have
+         lets take example of no 21 - 111
+    */
+
+    int smallestRepunitDivByK(int k) {
+        if(k%2==0 || k%5==0){
+            return -1;
+        }
+        int k1=k;
+        int cnt=1,cnt2=0;
+        while(k1){
+            int c=k1%10;
+            if(c==1){
+                cnt2++;
+            }
+            k1/=10;
+            cnt++;
+        }
+        if(cnt2==cnt-1){
+            return cnt2;
+        }
+        string s="";
+        for(int i=0;i<cnt;i++){
+            s.push_back('1');
+        }
+        int n=stoi(s);
+        int a=n%k;
+        cout<<a<<" ";
+        if(!a) return s.size();
+        int ad=0;
+        while(a!=0){
+            string s1=to_string(a);
+            s1.push_back('1');
+            n=stoi(s1);
+            a=n%k;
+            ad++;
+        }
+        return s.size()+ad;
+    }
+};
